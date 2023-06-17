b52fa8cd1844b0276aff8db09d24a2a2b123cc72
Boyan Donchev <boyan.donchev@wolfpack-dcs.com>
2023-06-17 17:16:12 +0300

    final commit

--- index.js
+++ index.js
@@ -5,13 +5,16 @@ const zahari = fs.readFileSync('zahari.txt', 'utf8')
 const input = fs.readFileSync('input.txt', 'utf8')
 const output = {aleko: 0, zahari: 0}
 
+// this function takes text and returns frequency table for the words in the text.
 function countWords(text) {
+  // remove special characters from the text, so we can compare only the words. (regex used)
   var wordArr =
     text.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g, '').
     split(' ')
 
   const res = {}
 
+  // fill the freq table.
   wordArr.forEach(word => {
     word = word.toLowerCase()
     if (res[word]) res[word] += 1
@@ -21,18 +24,26 @@ function countWords(text) {
   return res
 }
 
+
 function calssify() {
+  // take the freq tables of the 3 texts. (two sample texts and input text)
   const alekoMap = countWords(aleko)
   const zahariMap = countWords(zahari)
   const inputMap = countWords(input)
 
+  // object to store result coefficient.
   let res = {aleko: 0, zahari: 0}
 
+  // iterate through the input different words to compare them with the sample freq tables and change the coef accordingly.
   Object.keys(inputMap).forEach(curr => {
+    // if the word is not found in both sample texts, nothing happens.
 		if (!alekoMap[curr] && !zahariMap[curr]) {
       return
     }
 
+    // if the word is found in only one of the sample texts, the coef of the author is increased with the count of that word in the input text, two times.
+
+    // two times because if it is written by only one of the authors, it's much more likely to be authored by him.
     if (!alekoMap[curr]) {
 			res.zahari += 2*inputMap[curr]
 			return
@@ -43,14 +54,17 @@ function calssify() {
 			return
 		}
 
+    // if the word occurs in both sample texts we increase the author coef of the one with more occurences, by the number of occ in the input.
     if (alekoMap[curr] < zahariMap[curr]) res.zahari += inputMap[curr]
     else if (alekoMap[curr] > zahariMap[curr]) res.aleko += inputMap[curr]
 		else {
+      // if it is equal for both authors, we increase both coef by one.
 			res.aleko++
 			res.zahari++
 		}
   });
 
+  // transform the coefficients into percentages.
   let sum = (res.aleko + res.zahari) || 1
   output.aleko = Math.round( res.aleko / sum * 100 )
   output.zahari = Math.round( res.zahari / sum * 100 )
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+  output.aleko = Math.round( res.aleko / sum * 100 )
+  output.zahari = Math.round( res.zahari / sum * 100 )
